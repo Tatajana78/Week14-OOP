27cb8096841f42b356985a8aca24e5d599b34de0
Tanja Levcenko <tanjox78@gmail.com>
2023-12-14 11:10:50 +0300

    add my first app

---
+++ Week14-OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Dog goodBoy = new Dog("Good Boy");
+
+Console.WriteLine($"Name: {goodBoy.Name}; Level of happinnes: {goodBoy.LevelOfHappinness}");
+
+
+
+
+
+class Dog
+{
+    private string Name;
+    private int LevelOfHappinness;
+
+    public Dog(string name)
+    {
+        Name = name;
+        LevelOfHappinness = 0;
+        Console.WriteLine($"{Name} is your friend now.");
+    }
+}
